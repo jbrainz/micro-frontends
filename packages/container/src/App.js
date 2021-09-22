2dc1f5dcaaaba2a4d2190e1b041b5a52d41ac2d0
jbrainz <jonzyp4reala@gmail.com>
2021-09-22 12:54:47 +0100

    (MFE) chore : added the invalidations for aws distribution.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp'
 
 export default () => {
   return <div>
-    <h1>Hi There welcome!!</h1>
+    <h1>Hi There welcome back to MFE!!</h1>
     <hr />
     <MarketingApp />
   </div>
